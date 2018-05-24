57ef7e0d1d98adb6245cec80eda14b9e37d403a6
KorneliaAdamczyk <adamczykkornelia1@gmail.com>
2018-05-24 22:46:01 +0200

    19.1 Develop frontend of API - corrected2

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 $(document).ready(function() {
 
-  var apiRoot = 'https://sleepy-beach-61831.herokuapp.com/v1/task/';
-  var trelloApiRoot = 'https://sleepy-beach-61831.herokuapp.com/v1/trello/';
-  var datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
-  var tasksContainer = $('[data-tasks-container]');
+  const apiRoot = 'https://sleepy-beach-61831.herokuapp.com/v1/task/';
+  const trelloApiRoot = 'https://sleepy-beach-61831.herokuapp.com/v1/trello/';
+  const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
+  const tasksContainer = $('[data-tasks-container]');
+
+  var availableBoards = {};
+  var availableTasks = {};
 
   // init
 
